0d2b97613991a00e8050a83b6369300d010ad8ac
jibin <136713780@qq.com>
2018-10-15 22:03:07 +0800

    10.15 1

--- src/Leetcode/Easy/MinStack.java
+++ src/Leetcode/Easy/MinStack.java
@@ -1,12 +1,42 @@
 package Leetcode.Easy;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * 注意min栈的维护方法，比较巧妙
+ */
 public class MinStack {
 
+    public Stack<Integer> min ;
+    public Stack<Integer> st ;
     public MinStack() {
-        Stack<Integer> min =new Stack<>();
-        Stack<Integer> st =new Stack<>();
+        min =new Stack<>();
+        st =new Stack<>();
+    }
+    public void push(int x) {
+        st.push(x);
+        if (min.isEmpty()){
+            min.push(x);
+        }else {
+            if (x<min.peek()){
+                min.push(x);
+            }else {
+                min.push(min.peek());
+            }
+        }
+        return;
+    }
+
+    public void pop() {
+        st.pop();
+        min.pop();
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
     }
 }
