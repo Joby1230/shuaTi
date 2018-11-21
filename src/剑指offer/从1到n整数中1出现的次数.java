fde2797fa3153cd9e0078aaef179366b618d8c8f
jibin <136713780@qq.com>
2018-11-21 22:28:01 +0800

    18.11.21

--- src/剑指offer/从1到n整数中1出现的次数.java
+++ src/剑指offer/从1到n整数中1出现的次数.java
@@ -7,7 +7,21 @@ package 剑指offer;
  * 二、从数字规律着手
  */
 public class 从1到n整数中1出现的次数 {
+    public static void main(String[] args) {
+        从1到n整数中1出现的次数 a=new 从1到n整数中1出现的次数();
+        System.out.println(a.NumberOf1Between1AndN_Solution(13));
+    }
+    //笨方法
     public int NumberOf1Between1AndN_Solution(int n) {
-
+        int ans=0;
+        for (int i = 0; i <=n ; i++) {
+            String tmp = String.valueOf(i);
+            for (int j = 0; j <tmp.length() ; j++) {
+                if (tmp.charAt(j)=='1'){
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 }
